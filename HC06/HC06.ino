cada3b2083b6356c93e0b5749fdca59cbf8c385a
Theophile-Wemaere <theophile.wemaere@gmail.com>
2022-10-18 09:37:01 +0200

    added HC06 code

---
+++ HC06/HC06.ino
@@ -0,0 +1,21 @@
+
+void setup() {
+  //Initialize Serial Monitor
+  Serial.begin(9600);
+  Serial1.begin(9600);
+}
+
+void loop() 
+{
+  //Write data from HC06 to Serial Monitor
+  if(Serial.available())
+  {
+    String command = Serial.readString();
+    Serial.println("\nSending command : " + command);
+    Serial1.print(command);
+  }
+  if (Serial1.available()) 
+  {
+    Serial.write(Serial1.read());
+  }
+}
